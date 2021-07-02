718a381d15d477b4c1382c7d594039787214a0c3
Viperinius <viperinius@gmx.de>
2021-07-02 23:41:55 +0200

    delete out/ dir

--- vscode-ptxdist/out/generalConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PtxCommandsProvider = void 0;
-class PtxCommandsProvider {
-    constructor(parameters) {
-    }
-}
-exports.PtxCommandsProvider = PtxCommandsProvider;
-//# sourceMappingURL=generalConfig.js.map
